9ac11059d7490be12418a5a27f618bb4623b1f41
androidpirate <emrekalaycidesign@gmail.com>
2018-11-13 00:18:15 -0500

    Add book title, cover image and authors for each book

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class Book extends Component {
+  static propTypes = {
+    book: PropTypes.object.isRequired
+  }
+
   render() {
+    const bookCover = this.props.book.imageLinks && this.props.book.imageLinks.thumbnail
+    const bookTitle = this.props.book.title
+
+    let currentShelf = 'none'
+    for(let item of this.props.books) {
+      if(item.id === this.props.books) {
+        currentShelf = item.shelf
+        break
+      }
+    }
+
     return (
       <li>
         <div className="book">
           <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+            <div className="book-cover" style={{
+              width: 128,
+              height: 193,
+              backgroundImage: `url(${bookCover})` }}></div>
             <div className="book-shelf-changer">
-              <select>
+              <select defaultValue={currentShelf}>
                 <option value="move" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
@@ -17,8 +36,11 @@ class Book extends Component {
               </select>
             </div>
           </div>
-          <div className="book-title">To Kill a Mockingbird</div>
-          <div className="book-authors">Harper Lee</div>
+          <div className="book-title">{bookTitle}</div>
+          {this.props.book.authors &&
+            this.props.book.authors.map((author, index) =>
+              <div className="book-authors" key={index}>{author}</div>
+          )}
         </div>
       </li>
     )
